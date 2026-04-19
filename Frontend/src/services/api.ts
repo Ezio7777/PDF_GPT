9474eb325fe94a88e436612a45656f095d877025
Sunit Pal <sunitpal2000@gmail.com>
2026-04-19 18:07:47 +0530

    Frontend 1/2

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import { store } from '@/store/store'
 // import { logout } from '@/store/slices/authSlice'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL?.trim(),
   headers: { 'Content-Type': 'application/json' },
 })
 
